555b2abac70b101b1e4e7e24a8dd048da89b2807
Tryston <trystonproffit@gmail.com>
2026-02-20 00:52:02 -0500

    Added filtering and searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
-import ProjectList from "./components/ProjectList";
+import ProjectList from "./components/ProjectList.jsx";
+import SearchBar from "./components/SearchBar.jsx";
 
 function App() {
-    const [projects, setProjects] = useState([
+  const [searchTerm, setSearchTerm] = useState("");  
+  
+  const [projects, setProjects] = useState([
         { id: 1, title: "Project 1", description: "Project Description"},
         { id: 2, title: "Project 2", description: "Project Description"}
     ]);    
+
+    const filteredProjects = projects.filter(project =>
+      project.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <div>
             <h1>Project Showcase App</h1>
 
-            <ProjectList projects={projects} />
-            
+            <ProjectList projects={filteredProjects} />
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
     );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+function SearchBar({ searchTerm, setSearchTerm }) {
+    return (
+        <input 
+            type="text" 
+            placeholder="Search Projects"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
+    );
+}
+
+export default SearchBar;
